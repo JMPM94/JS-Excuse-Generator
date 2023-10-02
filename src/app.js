2e1d4779f5e8c9c764e6a97ec2ba6ccf164ead94
JMPM94 <137455695+JMPM94@users.noreply.github.com>
2023-10-02 19:51:43 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excusegenerator();
 };
+
+let excusegenerator = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = Math.floor(Math.random() * who.length);
+  let accion = Math.floor(Math.random() * action.length);
+  let que = Math.floor(Math.random() * what.length);
+  let cuando = Math.floor(Math.random() * when.length);
+
+  return (
+    who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando]
+  );
+};
+
+let refresh = document.getElementById("refresh");
+refresh.addEventListener("click", _ => {
+  location.reload();
+});
