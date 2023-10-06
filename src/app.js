61048b8ff6f7ce4fabf1727fc89af491bead3dd0
JMPM94 <137455695+JMPM94@users.noreply.github.com>
2023-10-06 18:14:19 +0000

    Third Commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,6 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  document.querySelector("#excuse").innerHTML = excusegenerator();
-};
 
 let excusegenerator = () => {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
@@ -21,25 +13,19 @@ let excusegenerator = () => {
     "during my lunch",
     "while I was praying"
   ];
-
-  // let quien = Math.floor(Math.random() * who.length);
   let quien = ObtenerElementoAleatorio(who);
   let accion = ObtenerElementoAleatorio(action);
   let que = ObtenerElementoAleatorio(what);
   let cuando = ObtenerElementoAleatorio(when);
-
   return (
     who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando]
   );
 };
-
 let refresh = document.getElementById("refresh");
 refresh.addEventListener("click", _ => {
   location.reload();
 });
-
 function ObtenerElementoAleatorio(ArrayCalcular) {
-  console.log("ObtenerElementoAleatorio");
-
   return Math.floor(Math.random() * ArrayCalcular.length);
 }
+document.querySelector("#excuse").innerHTML = excusegenerator();
