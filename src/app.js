288998af2f04a814b1b28d3c92653a219b19e70b
JMPM94 <137455695+JMPM94@users.noreply.github.com>
2023-10-06 19:17:38 +0000

    Fourth Commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 /* eslint-disable */
 import "bootstrap";
+import "./style.css";
+import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let excusegenerator = () => {
+let excuseGenerator = () => {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
@@ -13,19 +15,25 @@ let excusegenerator = () => {
     "during my lunch",
     "while I was praying"
   ];
-  let quien = ObtenerElementoAleatorio(who);
-  let accion = ObtenerElementoAleatorio(action);
-  let que = ObtenerElementoAleatorio(what);
-  let cuando = ObtenerElementoAleatorio(when);
+  let whoRandom = getRandomElement(who);
+  let actionRandom = getRandomElement(action);
+  let whatRandom = getRandomElement(what);
+  let whenRandom = getRandomElement(when);
   return (
-    who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando]
+    who[whoRandom] +
+    " " +
+    action[actionRandom] +
+    " " +
+    what[whatRandom] +
+    " " +
+    when[whenRandom]
   );
 };
 let refresh = document.getElementById("refresh");
 refresh.addEventListener("click", _ => {
   location.reload();
 });
-function ObtenerElementoAleatorio(ArrayCalcular) {
+function getRandomElement(ArrayCalcular) {
   return Math.floor(Math.random() * ArrayCalcular.length);
 }
-document.querySelector("#excuse").innerHTML = excusegenerator();
+document.querySelector("#excuse").innerHTML = excuseGenerator();
